b644795ded74770730c3aa5fe0cc3bb77b93dcc1
Jacek <jgrzybek5@gmail.com>
2022-07-13 11:57:58 +0200

    Second test - check search field

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
+const TIMEOUT = 3000000
 
 async function Test1() {
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get("https://www.danfoss.com/en/");
     await driver.findElement(By.xpath("//*[contains(text(),'Explore Danfoss Product Store')]"));
     driver.quit();
+}
+
+async function Test2() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get("https://www.danfoss.com/en/");
+    
+    await driver.manage().setTimeouts( {implicit: TIMEOUT, pageLoad: TIMEOUT, script: TIMEOUT});
+    var acceptCookie = await driver.findElement(By.className("coi-banner__accept"));
+    await acceptCookie.click();
+    var searchField = await driver.findElement(By.id("header-search-input"));
+
+    await searchField.sendKeys("Alsense\n");
+    await driver.findElement(By.xpath("//*[contains(text(),'Alsense IoT cloud and monitoring')]"));
+    await driver.findElement(By.xpath("//*[contains(text(),'.pdf')]"));
+    await driver.quit();
 }
