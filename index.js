0e04d8e3d95fa4724ce1c7cb2baf8d20de49b345
Jacek <jgrzybek5@gmail.com>
2022-07-13 11:56:32 +0200

    First test - check if string exist

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
+
+async function Test1() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.get("https://www.danfoss.com/en/");
+    await driver.findElement(By.xpath("//*[contains(text(),'Explore Danfoss Product Store')]"));
+    driver.quit();
+}
